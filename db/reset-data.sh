0cbc322c12825b171b1764714d52394e3c5ac2f1
estraier <hirarin@gmail.com>
2025-08-10 02:52:48 +0900

    Modify db initializers

--- db/reset-data.sh
+++ db/reset-data.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 set -a
-source ../.env
+source .env
 set +a
 
 docker-compose exec redis redis-cli -a $FAKEBOOK_REDIS_PASSWORD FLUSHALL
 
 docker-compose exec db psql -U $FAKEBOOK_DATABASE_USER $FAKEBOOK_DATABASE_NAME -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
 
-for f in ../db/init/*.sql; do
-  echo "Applying $f"
-  docker-compose exec -T db psql -U fakebook fakebook < "$f"
-done
+docker-compose exec -T db psql -U fakebook fakebook < db/init/01-schema.sql
+docker-compose exec -T db psql -U fakebook fakebook < db/init/02-basic-data.sql
