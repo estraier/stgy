46c261aee48628263666c05c61765dc73c8b6d2a
estraier <hirarin@gmail.com>
2025-09-15 19:49:35 +0900

    Use docker compose.

--- scripts/reset-cache.sh
+++ scripts/reset-cache.sh
@@ -4,6 +4,6 @@ set -a
 source .env
 set +a
 
-docker-compose exec redis redis-cli -a $STGY_REDIS_PASSWORD FLUSHALL
+docker compose exec redis redis-cli -a $STGY_REDIS_PASSWORD FLUSHALL
 
 echo "==> Redis reset done."
--- scripts/reset-data.sh
+++ scripts/reset-data.sh
@@ -4,10 +4,10 @@ set -a
 source .env
 set +a
 
-docker-compose exec db psql -U $STGY_DATABASE_USER $STGY_DATABASE_NAME -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
+docker compose exec db psql -U $STGY_DATABASE_USER $STGY_DATABASE_NAME -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
 
 ls db/init/*.sql | sort | while read file ; do
-  docker-compose exec -T db psql -U $STGY_DATABASE_USER $STGY_DATABASE_NAME < $file
+  docker compose exec -T db psql -U $STGY_DATABASE_USER $STGY_DATABASE_NAME < $file
 done
 
 echo "==> Postgres reset done."
