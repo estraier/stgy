751f3b9bdd7a8d249c583fc7b3c819ece4290552
estraier <hirarin@gmail.com>
2026-07-16 17:38:19 +0900

    modify scripts

--- scripts/run-storageUtil.sh
+++ scripts/run-storageUtil.sh
@@ -10,7 +10,16 @@ set -a
 [ -f .env ] && source .env
 set +a
 
-export STGY_STORAGE_S3_ENDPOINT=http://localhost:9000
+MINIO_PORT="${STGY_MINIO_PORT:-9000}"
+if ! [[ "$MINIO_PORT" =~ ^[0-9]+$ ]] || (( MINIO_PORT < 1 || MINIO_PORT > 65535 )); then
+    echo "invalid STGY_MINIO_PORT: $MINIO_PORT" >&2
+    exit 1
+fi
+export STGY_STORAGE_S3_ENDPOINT="http://localhost:${MINIO_PORT}"
+
+run_storage_util() {
+    npm run backend:storage-util -- "$@"
+}
 
 if [ $# -eq 1 ] && [ $1 = "volume" ]
 then
@@ -18,31 +27,31 @@ then
     while [ $i -lt 333 ] ; do
         i=$((i+1))
         name=$(printf "test-%05d.json" $i)
-        echo $name
-        npm run backend:storage-util save stgy-test:/$name package.json
+        echo "$name"
+        run_storage_util save "stgy-test:/$name" package.json
     done
     exit
 fi
 
 if [ $# -eq 1 ] && [ $1 = "test" ]
 then
-   npm run backend:storage-util save stgy-test:/test-package.json package.json
-   npm run backend:storage-util head stgy-test:/test-package.json
-   npm run backend:storage-util list stgy-test:/test-package
-   npm run backend:storage-util load stgy-test:/test-package.json tmp-package.json
+   run_storage_util save stgy-test:/test-package.json package.json
+   run_storage_util head stgy-test:/test-package.json
+   run_storage_util list stgy-test:/test-package
+   run_storage_util load stgy-test:/test-package.json tmp-package.json
    diff backend/package.json backend/tmp-package.json
-   npm run backend:storage-util delete stgy-test:/test-package.json
-   npm run backend:storage-util list stgy-test:/test-package
-   npm run backend:storage-util presigned-post stgy-test:/test-package.json tmp-package.json
-   npm run backend:storage-util head stgy-test:/test-package.json
-   npm run backend:storage-util copy stgy-test:/test-package.json stgy-test:/test-package-copied.json
-   npm run backend:storage-util head stgy-test:/test-package-copied.json
-   npm run backend:storage-util move stgy-test:/test-package-copied.json stgy-test:/test-package-moved.json
-   npm run backend:storage-util head stgy-test:/test-package-moved.json
-   npm run backend:storage-util delete stgy-test:/test-package.json
-   npm run backend:storage-util delete stgy-test:/test-package-moved.json
+   run_storage_util delete stgy-test:/test-package.json
+   run_storage_util list stgy-test:/test-package
+   run_storage_util presigned-post stgy-test:/test-package.json tmp-package.json
+   run_storage_util head stgy-test:/test-package.json
+   run_storage_util copy stgy-test:/test-package.json stgy-test:/test-package-copied.json
+   run_storage_util head stgy-test:/test-package-copied.json
+   run_storage_util move stgy-test:/test-package-copied.json stgy-test:/test-package-moved.json
+   run_storage_util head stgy-test:/test-package-moved.json
+   run_storage_util delete stgy-test:/test-package.json
+   run_storage_util delete stgy-test:/test-package-moved.json
    rm -f backend/tmp-package.json
    exit
 fi
 
-npm run backend:storage-util "$@"
+run_storage_util "$@"
